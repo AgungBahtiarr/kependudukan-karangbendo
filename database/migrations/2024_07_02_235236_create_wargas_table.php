db00cd681c9c1f7d18a00957552c364454328711
AgungBahtiarr <agungbahtiar050@gmail.com>
2024-07-03 06:58:01 +0700

    feat(database): add warga table

---
+++ database/migrations/2024_07_02_235236_create_wargas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wargas', function (Blueprint $table) {
+            $table->id();
+            $table->string('no_registrasi',30);
+            $table->string('nik',16)->unique();
+            $table->string('nkk',16);
+            $table->string('name',50);
+            $table->string('jabatan',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wargas');
+    }
+};
