d2ee56538d2e6060a24455fb6938e65bf8ee5730
Azizatur Rohma <azizaturrohmablt234@gmail.com>
2024-07-24 21:36:02 +0700

    Add Dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -2,13 +2,50 @@
 
 @section('title', $title)
 
-
 @section('content')
-    @role('Admin')
-        <p>Ini dashboard Admin</p>
-    @endrole
 
-    @role('Kader')
-        <p>Ini dashboard Kader</p>
-    @endrole
+@role('Admin')
+<div class="row mx-1 my-1">
+    <div class="col-sm-6">
+        <div class="card card-block card-stretch card-height">
+            <div class="card-body">
+                <div class="top-block d-flex align-items-center justify-content-between mb-3">
+                    <h3 class="text-primary">
+                        <span class="counter">{{ $jumlahWarga }}</span> Orang
+                    </h3>
+                    <div class="bg-primary icon iq-icon-box-2 mr-2 rounded">
+                        <i class="ri-team-fill"></i>
+                    </div>
+                </div>
+                <h4>Total Seluruh Warga</h4>
+                <div class="mt-1">
+                    <p class="mb-0">{{ date('d F Y', time()) }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6">
+        <div class="card card-block card-stretch card-height">
+            <div class="card-body">
+                <div class="top-block d-flex align-items-center justify-content-between mb-3">
+                    <h3 class="text-success">
+                        <span class="counter">{{ $jumlahCatatanKeluarga }}</span> Catatan Keluarga
+                    </h3>
+                    <div class="bg-success icon iq-icon-box-2 mr-2 rounded">
+                        <i class="ri-home-4-fill"></i>
+                    </div>
+                </div>
+                <h4>Total Catatan Keluarga</h4>
+                <div class="mt-1">
+                    <p class="mb-0">{{ date('d F Y', time()) }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endrole
+
+@role('Kader')
+<p>Ini dashboard Kader</p>
+@endrole
 @endsection
